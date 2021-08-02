26958e1d64992f7684904f0fe33cb3cde2c633a2
Ilya Aleichik <Ilya.alejchik@outlook.com>
2021-08-03 00:02:52 +0300

    adding files

---
+++ AccountingConsumables/Views/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AccountingConsumables.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Eqepment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Materials_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Expenses_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Orders_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Providers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Category_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Departament_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TypeMaterial_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitFromApp(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
